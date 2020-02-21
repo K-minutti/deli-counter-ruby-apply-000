b0de89a66e03c3b0aafe5cb701d958f2900b003d
Kevin Minutti <minutti70@gmail.com>
2020-02-21 14:26:18 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,18 +1,21 @@
+
 katz_deli = []
 
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    line_spot = [] #why do you need an empty array
+    line_spot = []
     counter = 1
-    katz_deli.each {|customer| line_spot.push("#{counter}. #{customer}") #what does the code in the .push parantheses do?
-    counter += 1} #why did you add += to the counter what function does it serve
-  puts "The line is currently: #{line_spot.join(" ")}" #why did you add the .join method?
+    katz_deli.each {|customer| line_spot.push("#{counter}. #{customer}")
+    counter += 1}
+    puts "The line is currently: #{line_spot.push(" ")}"
   end
 end
+
+
 def take_a_number(katz_deli, customer)
-  katz_deli.push(customer) #adding customer names to an array
+  katz_deli.push(customer)
   puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
 end
 
@@ -20,7 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}"
+    puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
 end
